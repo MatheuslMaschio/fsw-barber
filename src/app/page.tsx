efe781adb8b88fca43c2e8e0989679b0baecbad9
Matheus Maschio <matheuslmaschi0@hotmail.com>
2025-05-13 20:37:13 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button"
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button>Teste</Button>
     </div>
-  );
+  )
 }
